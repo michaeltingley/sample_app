eab7de25c8ef86c8741e5edaf0b1531c3f754701
Michael Tingley <tingley@tingley-Dell-System-XPS-L702X.(none)>
2012-11-28 15:10:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b788cb32edf3369d2fc58a81448e603b6f65d9575e61838faa68576a6c10c63b2c4d84065bdc361f012c6523ac242d1ac2d21b75854d5b51e936032264f42b7c'
