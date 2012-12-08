669a434ddb21fbb9d3b8115b8f08cad41e10f80e
Michael Tingley <MichaelTingley@College.Harvard.edu>
2012-12-08 03:53:17 -0500

    Lol easy chapter..

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def fullTitle(pageTitle)
+    baseTitle = "Tingley on Rails"
+    if pageTitle.empty?
+      baseTitle
+    else
+      baseTitle + " | " + pageTitle
+    end
+  end
 end
